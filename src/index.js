28590c6513b14056f56a5bc34d994625fab49a6c
Pedro Reyna III <p.reyna3@gmail.com>
2023-03-23 11:41:57 -0500

    Added Pete to text msg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send({msg:'Hello There'})
+    res.send({msg:'Hello There Pete!'})
   })
   
   app.listen(port, () => {
