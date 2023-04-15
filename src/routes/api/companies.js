3d19d45c3ec5a9383624a444669da654365f3002
Pedro Reyna III <p.reyna3@gmail.com>
2023-04-15 17:01:02 -0500

    added companies

---
+++ src/routes/api/companies.js
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { Router } from 'express'
+import { getCompanies, getCompany, addCompany } from '../../models/companies'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const companies = await getCompanies()
+  res.send(companies)
+})
+
+router.get('/:id', async (req, res) => {
+  const company = await getCompany(req.params.id)
+  if (company){
+    res.send(company)
+  } else {
+    res.status(404).send({msg:'company not found'})
+  }
+})
+
+router.post('/', async (req, res) => {
+  const companyName = req.body.name
+  if (companyName){
+  const company = await addCompany(companyName)
+  res.send(company)
+  }else {
+    res.status(400).send({msg:'company name is required'})
+  }
+})
+export default router
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import persons from './persons'
+import companies from './companies'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send({ msg: 'Inside API Endpoints' })
-  })
+  res.send({ msg: 'Inside API Endpoints' })
+})
 
 router.use('/persons', persons)
+router.use('/companies', companies)
+
 export default router
